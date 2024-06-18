e9d4b77b2f997e343dae7a30e1acfb24facda71f
Patryk Flama <51850536+PatrykFlama@users.noreply.github.com>
2024-06-18 17:48:04 +0200

    Update KodGraya.cpp

--- Algorytmy/Algebra/Systemy_liczbowe/Kod_Graya/KodGraya.cpp
+++ Algorytmy/Algebra/Systemy_liczbowe/Kod_Graya/KodGraya.cpp
@@ -3,9 +3,13 @@ using namespace std;
 
 
 int main(){
+	int exp, n;
+	cout << "Wykladnik: ";
+	cin >> exp;
+	n = 1<<exp;
 	for(int i = 0; i < n; i++){
 		int liczbaGreya = (i >> 1)^i;
-	    cout << bitset<4>(liczbaGreya).to_string() << '\n';
+	    cout << bitset<10>(liczbaGreya).to_string() << '\n';
 	    //i tu jeszcze trzeba znaleźć ktr liczbe zmieniasz i ja zmienic
 	}
 }
